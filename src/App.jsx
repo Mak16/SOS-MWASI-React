0dbd0714771fb19da1e028125c967e681e4ab493
makeda banza <makedabanza16@gmail.com>
2024-01-30 15:16:46 +0200

    deployement

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
-import './App.css'
-import Footer from './component/Footer';
-import Navigation from './component/Navigation';
-import Home from './component/Home';
-import About from './component/About';
-import { Route,Routes } from 'react-router-dom';
+import "./App.css";
+import Footer from "./component/Footer";
+import Navigation from "./component/Navigation";
+import Home from "./component/Home";
+import About from "./component/About";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-
   return (
     <>
-    <Navigation/>
-        <Routes>
-            <Route path='/react-project/' element={<Home/>} />
-            <Route path='/react-project/about' element={<About/>} />
-            <Route/>
-        </Routes>
-    <Footer/>
+      <Navigation />
+      <Routes>
+        <Route path="SOS-MWASI-React/" element={<Home />} />
+        <Route path="SOS-MWASI-React/about" element={<About />} />
+        <Route />
+      </Routes>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/component/Navigation.jsx
+++ src/component/Navigation.jsx
@@ -4,25 +4,32 @@ import Btn from "./Btn";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 function Navigation() {
-  let [isOpen,setIsOpen]=useState(false);
+  let [isOpen, setIsOpen] = useState(false);
   return (
     <>
       <nav className="flex  justify-between items-center bg-white py-[10px] px-[36px] text-sm ">
-        <Link to="/react-project/">
+        <Link to="SOS-MWASI-React/">
           <img
             className="sm:max-w-[200px] max-w-[100px] sm:pl-[5%] pl-2"
             src="src/assets/images/logo_sosmwasi 1.png"
             alt="logo S.O.S Mwasi"
           />
         </Link>
-        <div onClick={()=>setIsOpen(!isOpen)} className="">
-          {
-            isOpen?<Icon className="text-[#0096F5] block sm:hidden text-[2rem] sm:text-[46px]" icon="line-md:menu-to-close-alt-transition"/>:
-            <Icon className="text-[#0096F5] block sm:hidden text-[2rem] sm:text-[46px]" icon="mdi-light:menu"/>
-          }
+        <div onClick={() => setIsOpen(!isOpen)} className="">
+          {isOpen ? (
+            <Icon
+              className="text-[#0096F5] block sm:hidden text-[2rem] sm:text-[46px]"
+              icon="line-md:menu-to-close-alt-transition"
+            />
+          ) : (
+            <Icon
+              className="text-[#0096F5] block sm:hidden text-[2rem] sm:text-[46px]"
+              icon="mdi-light:menu"
+            />
+          )}
         </div>
         <ul className="w-[64%] hidden sm:flex flex-col justify-between items-center sm:flex-row md:flex-row lg:flex-row ">
-          <Link to="/react-project/about" className="list-none text-black">
+          <Link to="SOS-MWASI-React/about" className="list-none text-black">
             A propos
           </Link>
           <li className="list-none text-black">Que sont les VGB?</li>
@@ -34,7 +41,7 @@ function Navigation() {
           </li>
           <li>
             <Btn classBtn="bg-[#f5119c] font-bold text-white flex justify-between w-[225px] h-[70px] rounded-[10px] py-[10px] px-[25px]">
-              <div >
+              <div>
                 <span className="text-sm font-bold">Appel d'urgence</span>
                 <br />
                 <span className="text-sm font-bold">0825505783</span>
